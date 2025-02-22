b3502eaf69891c2be3a3040a72d752dc7bcf73b2
katoko137 <145273204+katoko137@users.noreply.github.com>
2025-02-22 13:02:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_templates
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Flask!"
+    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
