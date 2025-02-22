71076c633733279d4513ce5565598478780388db
katoko137 <145273204+katoko137@users.noreply.github.com>
2025-02-22 13:15:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
+from flask_socketio import SocketIO
 app = Flask(__name__)
-
+socketio = SocketIO(app)
 @app.route('/')
 def hello():
     return render_template("test.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debut=True)
